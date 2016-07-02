55c22e18e73d8042a8cb0d207c283334d04a78e4
John Humphreys <johnwilliamhumphreys@gmail.com>
2016-07-02 14:46:21 -0400

    Adding validation to post URL.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var _ = require("underscore");
 
 var port = process.env.PORT || 3000;
 
@@ -17,24 +18,28 @@ app.get('/todos', function(req, res) {
   res.json(todos);
 });
 
+//Refactored with underscore; finds single element where object matches.
 app.get('/todos/:id', function(req, res) {
-  var toDoId = req.params.id;
-  for (var i = 0; i < todos.length; ++i) {
-  	if (todos[i].id == req.params.id) {
-  		res.json(todos[i]);
-  		return;
-  	}
-  }
+  var toDoId = parseInt(req.params.id, 10);
+  var result = _.findWhere(todos, {id: toDoId});
+  if (result) res.json(result);
   res.status(404).send();	
 });
 
 //By convention, creating an item in REST uses the same
 //URL as the get-all URL, except that it is a post.
 app.post('/todos', function(req, res) {
-  var body = req.body;
+
+  var body = _.pick(req.body, "description", "completed");
+
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+  	return res.status(400).send();
+  }
+
+  body.description = body.description.trim();
+
   body["id"] = todoNextId++;
   todos.push(body);
-  console.log(todos);
   res.json(body);
 });
 
