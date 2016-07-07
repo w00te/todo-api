0822f89af6975dcca61924bfbd978f23e907a3cd
John Humphreys <johnwilliamhumphreys@gmail.com>
2016-07-07 19:54:15 -0400

    Added todo model and post todo and database.

---
+++ db.js
@@ -0,0 +1,14 @@
+//Sequelize is an ORM library.
+var Sequelize = require("Sequelize");
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+  "dialect": "sqlite",
+  "storage": __dirname + "/data/dev-todo-api.sqlite"
+});
+
+var db  = {};
+
+db.todo = sequelize.import(__dirname + "/models/todo.js");
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 var bodyParser = require("body-parser");
 var _ = require("underscore");
 
+//Create new sqlite database and load in to-do model and export db object.
+var db = require("./db.js")
+
 var port = process.env.PORT || 3000;
 
 var todos = [];
@@ -76,20 +79,29 @@ app.put("/todos/:id", function(req, res) {
 //By convention, creating an item in REST uses the same
 //URL as the get-all URL, except that it is a post.
 app.post('/todos', function(req, res) {
-
+  
   var body = _.pick(req.body, "description", "completed");
-
+  
   if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
   	return res.status(400).send();
-  }
+  }  
 
   body.description = body.description.trim();
 
-  body["id"] = todoNextId++;
-  todos.push(body);
-  res.json(body);
+  db.todo.create({
+    description: body.description.trim(),
+    completed: body.completed
+  }).then(function(todo) {
+    res.json(todo);
+  }).catch(function(e) {
+  	console.log(e);
+    return res.status(400).json(e);
+  });
 });
 
-app.listen(port, function() {
-  console.log("Express listening on port " + port + "!")
+//Sync creates the tables we defined if they do not already exist.
+db.sequelize.sync().then(function() {
+  app.listen(port, function() {
+    console.log("Express listening on port " + port + "!")
+  });
 });
