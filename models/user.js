9534d5d427aa3468f7fe3ee6d4012e07cbc406fd
John Humphreys <johnwilliamhumphreys@gmail.com>
2016-07-12 22:01:40 -0400

    Added sequelize hook to lower-case email to prevent case duplicates.

---
+++ models/user.js
@@ -0,0 +1,28 @@
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define("user", {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [7,100]
+      }
+    }
+  },
+  {
+    hooks: {
+      beforeValidate: function(user, options) {
+        if (typeof user.email === "string") {
+          user.email = user.email.toLowerCase()
+        }
+      }
+    }
+  });
+};
